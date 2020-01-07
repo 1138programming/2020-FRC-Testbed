e51716ea60af63fd6390fe4d5faaa2cac0079a12
Eagle Engineering <eeuser@chaminet.net>
2020-01-06 16:17:54 -0800

    Added Color Sensor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.Base;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -21,7 +21,6 @@ import frc.robot.subsystems.Base;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   public static RobotContainer oi;
-
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -43,6 +42,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+
+
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
--- src/main/java/frc/robot/subsystems/Base.java
+++ src/main/java/frc/robot/subsystems/Base.java
@@ -17,6 +17,14 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+
 public class Base extends SubsystemBase {
 
   public static final int KDriveRightTopTalon = 4;
@@ -32,6 +40,15 @@ public class Base extends SubsystemBase {
   private TalonSRX driveLeftFront;
   private TalonSRX driveRightRear; 
   private TalonSRX driveLeftRear;
+
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
   public Base() {
     driveRightTop = new TalonSRX(KDriveRightTopTalon);
@@ -49,10 +66,43 @@ public class Base extends SubsystemBase {
     driveRightRear.set(ControlMode.Follower, driveRightFront.getDeviceID());
     driveLeftTop.set(ControlMode.Follower, driveLeftFront.getDeviceID());
     driveLeftRear.set(ControlMode.Follower, driveLeftFront.getDeviceID());
+
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);  
   }
 
   @Override
   public void periodic() {
+    Color detectedColor = m_colorSensor.getColor();
+    double IR = m_colorSensor.getIR();
+
+    String colorString;
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    }
+    else if (match.color == kRedTarget) {
+      colorString = "Red";
+    }
+    else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    }
+    else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    }
+    else {
+      colorString = "Unknown";
+    }
+
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
+    SmartDashboard.putNumber("IR", IR);
     // This method will be called once per scheduler 
   }
 
